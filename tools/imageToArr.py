ddd4940ab0c1260ebb54162ac451af55ab17902b
Ilai Segev <ilaisgv@gmail.com>
2021-08-02 14:05:37 +0300

    changed imageToArr.py to a commandline tool

--- tools/imageToArr.py
+++ tools/imageToArr.py
@@ -1,23 +1,57 @@
 from PIL import Image
+import os
+import sys
 
-NAME = "deej_logo"
 
-img = Image.open("logo_black.png").convert("RGB")
-with open(NAME+".h", 'w') as f:
-    f.write(
-        f"__align(4) const unsigned char gImage_{NAME}[{img.width*img.height*4}] = ")
-    f.write("{\n")
+def convertImage(img, output_path):
+    try:
+        with open(os.path.splitext(output_path)[0] + ".h", 'w') as f:
+            f.write(
+                f"__align(4) const unsigned char gImage_{name}[{img.width*img.height*4}] = ")
+            f.write("{\n")
 
-    count = 0
-    pixel_values = ""
-    for y in range(img.height):
-        for x in range(img.width):
-            rgb = img.getpixel((x, y))
-            for value in reversed(rgb):
-                count += 1
-                f.write((hex(value).upper()) + ',')
-            count += 1
-            f.write("0X00,")
-            if(count % 16 == 0):
-                f.write('\n')
-    f.write("};\n")
+            count = 0
+            for y in range(img.height):
+                for x in range(img.width):
+                    rgb = img.getpixel((x, y))
+                    for value in reversed(rgb):
+                        count += 1
+                        f.write(f"{value:#04X},")
+                    count += 1
+                    f.write("0X00,")
+                    if(count % 16 == 0):
+                        f.write('\n')
+            f.write("};\n")
+    except PermissionError:
+        print(f"Permission denied: {os.path.splitext(output_path)[0] + '.h'}")
+
+
+def removesuffix(s, suffix):
+    if suffix and s.endswith(suffix):
+        return s[:-len(suffix)]
+    return s
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage : python imageToArr.py [input image] [output file]")
+        sys.exit(-1)
+    img_path = os.path.abspath(sys.argv[1])
+    output_path = os.path.abspath(sys.argv[2])
+
+    if not os.path.isfile(img_path):
+        print(img_path + " does not exist")
+        sys.exit(-1)
+
+    name = removesuffix(os.path.basename(output_path), ".h")
+    if not ("___"+name).isidentifier():
+        print(f"invalid variable name: {name}")
+        sys.exit(-1)
+
+    try:
+        img = Image.open(img_path).convert("RGB")
+    except OSError:
+        print(img_path + " is not an image file")
+        sys.exit(-1)
+    else:
+        convertImage(img, output_path)
